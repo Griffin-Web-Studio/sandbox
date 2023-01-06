e248bbee49772e5e3bf408adc648ec7b12eeb6d8
Rihards Simanovics <rihards.s@griffin-web.studio>
2023-01-06 00:04:54 +0000

    0.0.5.1 | small tweaks to Pref Color Mode UI/UX (fix CI/CD)

--- src/componenets/UI/CodemirrorInput.js
+++ src/componenets/UI/CodemirrorInput.js
@@ -2,7 +2,7 @@ import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { html } from "@codemirror/lang-html";
 import { githubDark } from "@uiw/codemirror-theme-github";
-import { gruvboxLight, gruvboxDark } from "@uiw/codemirror-theme-gruvbox-dark";
+import { gruvboxLight } from "@uiw/codemirror-theme-gruvbox-dark";
 
 export default function CodemirrorInput(props) {
     const { onInputChange, themeColor } = props;
