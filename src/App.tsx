c85fc14f02f69dc79eb47f705d633be9e46bbc63
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-08-03 22:43:45 +0000

    feat: implement logo preview

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,44 @@ import usePrefersColorScheme from "use-prefers-color-scheme";
 
 // Images
 import Logo from "@/assets/logo.svg?react";
+import LightDark from "@/assets/light-dark.svg?react";
 import GwsLogo from "@/assets/logo-garage.svg?react";
+import logo from "@/assets/logo.svg";
 
 function App() {
   const preferredColourScheme = usePrefersColorScheme();
-  const darkTheme = preferredColourScheme.match("dark");
+  const darkTheme = preferredColourScheme === "dark";
+  const [preferredDark, setPreferredDark] = React.useState<boolean>(
+    localStorage.getItem("preferredDark") === "true"
+  );
+  const [previewLoaded, setPreviewLoaded] = React.useState<boolean>(false);
+  const [codeBlock, setCodeBlock] = React.useState<string>("");
+
+  React.useEffect(() => {
+    fetch(logo)
+      .then((response) => response.text())
+      .then((text) => {
+        setCodeBlock(text);
+        setPreviewLoaded(true);
+      });
+  }, [setCodeBlock]);
+
+  React.useEffect(() => {
+    document.body?.setAttribute("pref-color", preferredDark ? "dark" : "light");
+  }, [preferredDark]);
+
+  const onPreferredThemeHandler = () => {
+    if (preferredDark) {
+      setPreferredDark(false);
+      localStorage.setItem("preferredDark", "false");
+      document.body?.setAttribute("pref-color", "light");
+      return;
+    }
+
+    setPreferredDark(true);
+    localStorage.setItem("preferredDark", "true");
+    document.body?.setAttribute("pref-color", "dark");
+  };
   return (
     <div className="gws-live-preview">
       <div className="grid gap-20">
@@ -29,9 +62,18 @@ function App() {
             </div>
 
             <div className="header__preferred-color grid-1 flex align-center">
-              {/* <button className={`button${currentReverseThemeMode === "light" ? " button__secondary button__secondary--radius" : " button--radius button--dark"}`} onClick={onButtonDarkModeHandler}>
-                                <LightDark className={`header__preferred-color-svg header__preferred-color-svg--${currentReverseThemeMode}`} />
-                            </button> */}
+              <button
+                className={`button button--radius ${
+                  preferredDark && " button--dark"
+                }`}
+                onClick={onPreferredThemeHandler}
+              >
+                <LightDark
+                  className={`header__preferred-color-svg header__preferred-color-svg--${
+                    preferredDark ? "light" : "dark"
+                  }`}
+                />
+              </button>
             </div>
 
             <div className="header__reset-code grid-1 flex align-center">
