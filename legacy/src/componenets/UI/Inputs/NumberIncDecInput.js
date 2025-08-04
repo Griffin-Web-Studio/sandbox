0f1e582afde074c56b93cf38d365de7adc9cd7eb
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-08-04 20:39:02 +0000

    chore: remove unused legacy code
    Likely from another project so has no reason to be here

--- legacy/src/componenets/UI/Inputs/NumberIncDecInput.js
+++
@@ -1,8 +0,0 @@
-// Input.js
-import React from 'react';
-
-export default function NumberIncDecInput(props) {
-  const { value, onChange } = props;
-
-  return <input type="number" value={value} onChange={onChange} readOnly />;
-};
