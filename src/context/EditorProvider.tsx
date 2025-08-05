7fdb248b7edf810cf0934b6dfcf3570c32f927b7
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-08-05 00:53:14 +0000

    fix: ensure initial localStorage value is set

--- src/context/EditorProvider.tsx
+++ src/context/EditorProvider.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import EditorContext, { type editorStoreValues } from "./EditorContext";
+import { getHtmlSample, getJsSample, getSvgSample } from "@/utils/sample";
 
 export interface EditorProviderProps {
   children: React.ReactNode;
 }
+
+if (!localStorage.getItem("html_code"))
+  localStorage.setItem("html_code", await getHtmlSample());
+if (!localStorage.getItem("svg_code"))
+  localStorage.setItem("svg_code", await getSvgSample());
+if (!localStorage.getItem("js_code"))
+  localStorage.setItem("js_code", await getJsSample());
 
 const defaultValue: editorStoreValues = {
   codeStore: {
