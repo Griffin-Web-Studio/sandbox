b09198ed65f635e5d8fafd84a48f3d519945d054
Rihards Simanovics <rihards.s@griffin-web.studio>
2025-08-26 11:45:04 +0000

    feat: update xml viewer to display actual xml

--- src/components/editor/XMLEditor.tsx
+++ src/components/editor/XMLEditor.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Editor from "../ui/Editor";
 import EditorContext from "@/context/EditorContext";
+import XMLViewer from "react-xml-viewer";
 
 // interface XMLEditorProps
 
@@ -13,7 +14,9 @@ const XMLEditor: React.FC = () => {
   const onEditorChangeHandler = React.useCallback(
     (value: string) => {
       if (!setEditorStore) return;
+
       localStorage.setItem("xml_code", value);
+
       setEditorStore((old) => ({
         ...old,
         codeStore: { ...old.codeStore, xml: value },
@@ -36,11 +39,13 @@ const XMLEditor: React.FC = () => {
       </div>
 
       <div className="grid-24 grid-lt-12">
-        <iframe
-          srcDoc={localStorage.getItem("xml_code") ?? ""}
-          className="gws-live-preview__code-preview flex align-center justify-center"
-          title="Preview Frame"
-        />
+        <div className="gws-live-preview__code-preview">
+          <XMLViewer
+            xml={editorStore?.codeStore.xml ?? ""}
+            collapsible={true}
+            showLineNumbers={true}
+          />
+        </div>
       </div>
     </>
   );
